456f7c30977c5c7eb99452e5e626f5bcc38623ca
Prince Thind <Princethind799@gmail.com>
2021-12-07 18:02:03 +0530

    implement basic form submit

--- script.js
+++ script.js
@@ -1,78 +1,84 @@
-const addBook = document.getElementById("add-book");
-const modal = document.getElementById("myModal");
+const addBookButton = document.getElementById("add-book");
 const bookForm = document.getElementById("form");
 const cancelBtn = document.getElementById("cancel-create-book");
-const submitBtn = document.getElementById("create-book");
-const appendElements=document.getElementById('book-templates');
-const bookContainer=document.querySelector('#book-templates');
 
+// const submitBtn = document.getElementById("create-book");
+// const appendElements = document.getElementById("book-templates");
+const bookContainer = document.querySelector("#book-templates");
 
-addBook.addEventListener("click", function () {
-  modal.style.display = "block";
-});
-cancelBtn.addEventListener("click", function () {
-  modal.style.display = "none";
-});
+addBookButton.addEventListener("click", toggleBookModal);
+cancelBtn.addEventListener("click", toggleBookModal);
 
-
-let myLibrary = [];
-function Book(title, author, pages,read) {
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.read=read
+function toggleBookModal() {
+  const Bookmodal = document.getElementById("myModal");
+  console.log(Bookmodal);
+  Bookmodal.classList.toggle("hide");
 }
 
-submitBtn.addEventListener("click", submitForm);
-function submitForm() {
-  let bookTitle = document.getElementById("input-book-title").value;
-  let bookAuthor = document.getElementById("input-book-author").value;
-  let bookPages = document.getElementById("input-book-pages").value;
-  let bookRead=document.mainform.read.value;
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
+}
+
+bookForm.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  const form=e.target; 
+
+  const bookTitle=form.title.value;
+  const bookAuthor=form.author.value;
+  const bookPages=form.pages.value;
+  const bookRead=form.read.value;
+
+  const book=new Book(bookTitle,bookAuthor,bookPages,bookRead);
+  myLibrary.push(Book);
+
  
-  // console.log(bookRead);
 
-  const book1 = new Book(bookTitle, bookAuthor, bookPages,bookRead);
-  myLibrary.push(book1);
-  modal.style.display = "none";
-  bookForm.reset();
-  console.log(myLibrary[myLibrary.length-1]);
+  return;
+ 
 
-  const box=document.createElement("div");
-  const titleOfBook=document.createElement("h3");
-  const authorOfBook=document.createElement("p");
-  const pagesOfBook=document.createElement("p");
-  const readBook=document.createElement("p");
-  const deleteIcon=document.createElement("div");
-  
-  box.classList.add('template-1');
-  titleOfBook.classList.add('template-heading');
-  authorOfBook.classList.add('template-optns');
-  pagesOfBook.classList.add('template-optns');
-  readBook.classList.add('read-div');
-  deleteIcon.classList.add('delete-icon')
+  const box = document.createElement("div");
+  const titleOfBook = document.createElement("h3");
+  const authorOfBook = document.createElement("p");
+  const pagesOfBook = document.createElement("p");
+  const readBook = document.createElement("p");
+  const deleteIcon = document.createElement("div");
 
-  titleOfBook.textContent=book1.title;
-  authorOfBook.textContent=book1.author;
-  pagesOfBook.textContent=book1.pages;
-  book1.read == ("Yes") ? readBook.innerText="Read" : readBook.innerText="Not Read";
-  
+  box.classList.add("template-1");
+  titleOfBook.classList.add("template-heading");
+  authorOfBook.classList.add("template-optns");
+  pagesOfBook.classList.add("template-optns");
+  readBook.classList.add("read-div");
+  deleteIcon.classList.add("delete-icon");
 
+  titleOfBook.textContent = book1.title;
+  authorOfBook.textContent = book1.author;
+  pagesOfBook.textContent = book1.pages;
+  book1.read == "Yes"
+    ? (readBook.innerText = "Read")
+    : (readBook.innerText = "Not Read");
 
-  deleteIcon.innerHTML='<i class="fas fa-trash-alt" id="trash-icon" onClick="deleteTemplate()"></i>'
+  deleteIcon.innerHTML =
+    '<i class="fas fa-trash-alt" id="trash-icon" onClick="deleteTemplate()"></i>';
 
   box.appendChild(deleteIcon);
   box.appendChild(titleOfBook);
   box.appendChild(authorOfBook);
   box.appendChild(pagesOfBook);
   box.appendChild(readBook);
-  
+
   appendElements.appendChild(box);
 
-  if(bookRead=='No'){
-    readBook.style.backgroundColor='red';
+  if (bookRead == "No") {
+    readBook.style.backgroundColor = "red";
   }
- 
 }
 // bookContainer.addEventListener('click',toggle)
 // function toggle(event){
@@ -86,14 +92,6 @@ function submitForm() {
 //   }
 // }
 
-
-
-
-function deleteTemplate(){
+function deleteTemplate() {
   alert("hi");
 }
-
-   
-
-
-
